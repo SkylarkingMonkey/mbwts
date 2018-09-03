692b1e738924cdc773e68cb2ca88002c4fbb34f5
skylarkingmonkey <SkylarkingMonkey@users.noreply.github.com>
2018-09-03 13:50:46 -0700

    db test

--- src/connect_to_mysql.php
+++ src/connect_to_mysql.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "mbwts_episodes";
+$dbname = "bitchin_episodes";
 
 $conn = new mysqli($servername, $username, $password, $dbname) or die("The Monkey was not Blended Properly. Please acquire a Vitamix and try again.");
 ?>
